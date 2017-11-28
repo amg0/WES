4d5355eedf4c6f1fe40b81944ce5176dc8ba9577
amg0 <alexis.mermet@gmail.com>
2017-11-28 22:44:10 +0100

    cosmetic change in JS setting tab

--- J_WES.js
+++ J_WES.js
@@ -83,7 +83,7 @@ function wes_Settings(deviceID) {
 	jQuery.each( configs, function(idx,obj) {
 		var value = get_device_state(deviceID,  wes_Svs, obj.name,1);
 		htmlConfigs += '	\
-					<div class="form-group col-xs-6">																	\
+					<div class="form-group col-xs-6 col-6">																	\
 						<label for="wes-{0}">{1}</label>		\
 						<input type="{3}" class="form-control" id="wes-{0}" placeholder="{2}" value="{4}">	\
 					</div>																										\
@@ -98,24 +98,24 @@ function wes_Settings(deviceID) {
 	var html =
     '                                                           \
       <div id="wes-settings">                                           \
-        <form id="wes-settings-form">                        \
-					<div class="form-group col-xs-6">																	\
+        <form class="row" id="wes-settings-form">                        \
+					<div class="form-group col-xs-6 col-6">																	\
 						<label for="wes-ipaddr">IP Addr</label>		\
 						<input type="text" class="form-control" id="wes-ipaddr" placeholder="xx.xx.xx.xx">	\
 					</div>																										\
-					<div class="form-group col-xs-6">																	\
+					<div class="form-group col-xs-6 col-6">																	\
 						<label for="wes-RefreshPeriod">Polling in sec</label>			\
 						<input type="number" min="1" max="600" class="form-control" id="wes-RefreshPeriod" placeholder="5">	\
 					</div> 																								\
-					<div class="form-group col-xs-6">																	\
+					<div class="form-group col-xs-6 col-6">																	\
 						<label for="wes-username">User Name</label>		\
 						<input type="text" class="form-control" id="wes-username" placeholder="User">	\
 					</div>																										\
-					<div class="form-group col-xs-6">																	\
+					<div class="form-group col-xs-6 col-6">																	\
 						<label for="wes-pwd">Password</label>			\
 						<input type="password" class="form-control" id="wes-pwd" placeholder="Password">	\
 					</div>	'+htmlConfigs+'																							\
-					<div class="form-group col-xs-12">																	\
+					<div class="form-group col-xs-12 col-12">																	\
 						<button id="wes-submit" type="submit" class="btn btn-primary">Submit</button>	\
 					</div>																										\
 				</form>                                                 \
